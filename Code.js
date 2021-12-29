b29808521683e5cd180e68449751b4ee0bba0807
Gumersiano111 <88902399+Gumersiano111@users.noreply.github.com>
2021-12-29 13:10:37 -0600

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "NICKI MINAJ";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("JANIM IKCIN", 100,50)
+    fill("lightblue")
+    text("Hint: The queen of rap", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Perfoms a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,310)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,330)
+}
+    
+
